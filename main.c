1c35ef3c895e2b01d9bdad34d0549fcd8d604c2a
Selim Efe <uysalselimefe@gmail.com>
2026-01-17 22:10:00 +0300

    Update main.c

--- main.c
+++ main.c
@@ -4,52 +4,60 @@
 
 int main()
 {
-        int sayi,i,n,sum,asal,j;
+    int sayi, i, n, sum, asal, j;
 
     printf("n i girin");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    sum=2;
+    sum = 2;
 
     srand(time(NULL));
 
-    for(i=0;i<n;i++){
-        for(;;){
-            sayi=rand()%100+1;
+    for (i = 0; i < n; i++)
+    {
+        for (;;)
+        {
+            sayi = rand() % 100 + 1;
 
-            if(sum%2==0){
-                asal=1;
-                if(sayi==1){
-                    asal=0;
+            if (sum % 2 == 0)
+            {
+                asal = 1;
+                if (sayi == 1)
+                {
+                    asal = 0;
                 }
-                for(j=2;j<sayi;j++){
-                    if(sayi%j==0){
-                        asal=0;
+                for (j = 2; j < sayi; j++)
+                {
+                    if (sayi % j == 0)
+                    {
+                        asal = 0;
                         break;
                     }
                 }
-                if(asal==1){
-                    printf("%d ",sayi);
+                if (asal == 1)
+                {
+                    printf("%d ", sayi);
                     sum++;
                     break;
                 }
-
-
             }
-            else{
-                asal=1;
-                for(j=2;j<sayi;j++){
-                    if(sayi%j==0){
-                        asal=0;
+            else
+            {
+                asal = 1;
+                for (j = 2; j < sayi; j++)
+                {
+                    if (sayi % j == 0)
+                    {
+                        asal = 0;
                         break;
                     }
                 }
-                if(asal==0){
-                    printf("%d ",sayi);
+                if (asal == 0)
+                {
+                    printf("%d ", sayi);
                     sum++;
                     break;
                 }
-
             }
         }
     }
